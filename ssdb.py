8e9659c03c5fa1c90f4f39ce0fd4a2dc8afa2d4e
est <electronicstar@126.com>
2013-02-17 16:21:17 +0800

    xslice parse

---
+++ ssdb.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import socket, os, sys, asynchat, functools
+
+class SSDBConn(asynchat.async_chat):
+    def __init__(self, sock, *args, **kwargs):
+        pass
+
+DEBUG = True
+NL = '\n'
+
+def xslice(s, a, b=None):
+    if b is None:
+        a, b = 0, a
+    if isinstance(a, (str, unicode)):
+        x = s.find(a)
+    elif isinstance(a, (int, float)):
+        x = int(a)
+    if isinstance(b, (str, unicode)):
+        y = s.find(b) + len(b)
+    elif isinstance(b, (int, float)):
+        y = int(b)
+    if x==-1 or y==-1:
+        return None
+    return s[x:y], y
+
+class SSDB(object):
+    addr = None
+    RECV_BUFFER_SIZE = 4096
+    SEND_BUFFER_SIZE = 4096
+    def __init__(self, *args, **kwargs):
+        """using either:
+         - ssdb://
+         - ip:port
+         - host='', port=''
+        """
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if len(args)==1:
+            t = args[0].partition('//'
+                )[2].rpartition('@'
+                )[2].rpartition(':')
+            self.addr = (
+                t[0].replace('[', '').replace(']', ''), 
+                t[2]
+            )
+        elif len(args) == 2:
+            self.addr = args
+        h, p = kwargs.get('host'), kwargs.get('port')
+        # handle default server address here
+        t = list(self.addr or ['127.0.0.1', 8888]) 
+        if h:
+            t[0] = h
+        if p:
+            t[1] = p
+        self.addr = t
+        self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        self.sock.connect(tuple(self.addr))
+        #self.conn = SSDBConn(self.sock)
+
+        commands = ['get', 'set', 'del', 'incr', 'decr', 'keys', 'scan', 'rscan', 
+        'multi_get', 'multi_set', 'multi_del', 
+        # hash
+        'hdecr', 'hdel', 'hget', 'hincr', 'hkeys', 'hlist', 'hrscan', 'hscan', 'hset', 'hsize', 
+        # sorted list
+        'zdecr', 'zdel', 'zget', 'zincr', 'zkeys', 'zlist', 'zrscan', 'zscan', 'zset', 'zsize']
+        for cmd in commands:
+            setattr(self, cmd, functools.partial(self.operation, cmd, ))
+    def parse(self, rbuffer):
+
+
+    def operation(self, cmd, *args):
+        sbuffer = [cmd]
+        sbuffer.extend(args)
+        sbuffer = NL.join(map(lambda x:'%d%s%s' % (len(x), NL, x), sbuffer)) + NL*2
+        if DEBUG: print sbuffer
+        bytes = self.sock.send(sbuffer)
+        if DEBUG: print '%s: %sBytes' % (cmd, bytes)
+        rtn = self.sock.recv(self.RECV_BUFFER_SIZE)
+        print repr(rtn)
+        loop = True:
+        result = []
+        cursor = 0
+        while loop:
+            l, cursor = xslice(rtn, cursor, NL)
+
+
+
+if __name__ == '__main__':
+    db = SSDB()
