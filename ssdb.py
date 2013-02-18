fb3bdd4801dcbcfaea796fcfb47d38fd3d2909a1
est <electronicstar@126.com>
2013-02-18 11:16:35 +0800

    parse lines

--- ssdb.py
+++ ssdb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import socket, os, sys, asynchat, functools
+import socket, os, sys, asynchat, functools, time
 from cStringIO import StringIO
 
 DEBUG = True
@@ -14,8 +14,8 @@ class SSDBConn(asynchat.async_chat):
         self.incoming.append(data)
 
     def found_terminator(self):
-        if isinstance(self.terminator, int):
-            result.append(self._get_data())
+        if isinstance(self.get_terminator(), int):
+            self.result.append(self._get_data())
 
 class SSDB(object):
     addr = None
@@ -46,9 +46,11 @@ class SSDB(object):
         if p:
             t[1] = p
         self.addr = t
+        # self.sock.setblocking(0)
         self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        # time.sleep(1)
         self.sock.connect(tuple(self.addr))
-        self.conn = SSDBConn(self.sock)
+        # self.conn = SSDBConn(self.sock)
 
         commands = ['get', 'set', 'del', 'incr', 'decr', 'keys', 'scan', 'rscan', 
         'multi_get', 'multi_set', 'multi_del', 
@@ -59,21 +61,6 @@ class SSDB(object):
         for cmd in commands:
             setattr(self, cmd, functools.partial(self.operation, cmd, ))
 
-    def parse(self, rbuffer):
-        result = []
-        cursor = 0
-        all_length = len(rbuffer)
-        while cursor < all_length:
-            c = rbuffer.find(NL)
-            if c==-1:
-                break
-            else:
-                x, y = cursor, c
-                r = rbuffer[cursor:c]
-                cursor = y + len(NL)
-                result.append(rbuffer[cursor:r])
-        return result
-
     def operation(self, cmd, *args):
         sbuffer = [cmd]
         sbuffer.extend(args)
@@ -82,27 +69,56 @@ class SSDB(object):
         bytes = self.sock.send(sbuffer)
         if DEBUG: print '%s: %sBytes' % (cmd, bytes)
 
-        loop = True
-        chunks = []
-        results = []
-        cursor = (0, 0)
+        incoming = bytearray() # the reading buffer
+        results = [] # outcome results
+        pos = 0 # current reading cursor position relative to incoming
+        tlen = 0 # total data read
 
-        while loop:
-            chunks.append(self.sock.recv(self.RECV_BUFFER_SIZE))
-            # if len(s) < self.RECV_BUFFER_SIZE:
-            #     continue
-            # else:
-            c = chunks[cursor[0]].find()
+        term_pos = True # assert there will be a term from the start
 
-        print repr(rtn)
-        loop = True
-        result = []
-        cursor = 0
-        while loop:
-            l, cursor = xslice(rtn, cursor, NL)
+        eof  = False # EOF not found
+        term = NL # either str NL or length
+
+        while not eof:
+            print 'loop, %r, %r, %r, %r' % (term, pos, term_pos, results)
+            if pos>tlen or pos==tlen==0:
+                data = self.sock.recv(self.RECV_BUFFER_SIZE)
+                incoming.extend(data)
+                tlen += len(data)
+            if isinstance(term, str):
+                term_pos = incoming.find(term, pos)
+                if term_pos==-1:
+                    if pos:
+                        incoming = bytearray(incoming[pos:])
+                    continue
+                elif term_pos==0:
+                    eof = True
+                    break
+                elif term_pos>0:
+                    print 'loop, %r, %r, %r, %r' % (term, pos, term_pos, results)
+                    print repr(incoming)
+                    next_term = int(incoming[pos:term_pos]) # length of the value field
+                    pos += term_pos+len(term)
+                    term = next_term
+            if isinstance(term, int):
+                if pos+term>tlen:
+                    print pos, term, tlen
+                    data = self.sock.recv(self.RECV_BUFFER_SIZE)
+                    incoming.extend(data)
+                    tlen += len(data)
+                    continue
+                else:
+                    results.append(incoming[pos:pos+term])
+                    pos += term + len(NL)
+                    term = NL
+        return results
+
+
 
 
 
 if __name__ == '__main__':
     import readline, rlcompleter; readline.parse_and_bind("tab: complete")
     db = SSDB()
+    # time.sleep(1)
+    db.incr('a')
